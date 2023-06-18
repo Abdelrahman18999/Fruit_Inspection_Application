c3f620110cfa6cdd11fdc83af119627a570b9b0b
Abdelrahman Ahmed <59202700+Abdelrahman18999@users.noreply.github.com>
2023-06-18 14:34:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import base64
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+from keras.models import load_model
+from util import set_background, classify
+
+st.title("Fruit Inspection Application")
+st.markdown("This web application about classify fruits if it is fresh or spoild")
+st.header("Upload a fruit image")
+
+# Set a background image
+set_background("fruit_background.jpeg")
+
+# upload an image file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+# Load the saved model
+model = load_model("fruits_inspection_model.h5")
+
+#classify(image, model)
+
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+    # classify image
+    index, conf_score = classify(image, model)
+    
+    # write classification
+    st.write("## {}".format(index))
+    st.write("### score: {}%".format(int(conf_score * 100) / 100))
+
